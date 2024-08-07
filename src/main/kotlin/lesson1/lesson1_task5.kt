90a429f3efd7429b28ff86fb95bec158cea347fd
alexglazyrin <alex_glazyrin@yahoo.com>
2024-08-07 19:26:36 +0300

    KS-1-5 fix smth

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -3,9 +3,9 @@ package org.example.lesson1
 fun main() {
 
     val flyingGagarinInSeconds = 6480 // Нашел только про 108 минут информацию.
-    val hour = flyingGagarinInSeconds / 3600
-    val minutes = flyingGagarinInSeconds % 3600 / 60
-    val seconds = flyingGagarinInSeconds % 3600 % 60
+    val hour = String.format("%02d", (flyingGagarinInSeconds / 3600))
+    val minutes = String.format("%02d", (flyingGagarinInSeconds % 3600 / 60))
+    val seconds = String.format("%02d", (flyingGagarinInSeconds % 3600 % 60))
 
     println("Полет Ю. Гагарина длился: $hour:$minutes:$seconds")
 }
