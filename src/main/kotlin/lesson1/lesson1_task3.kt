ee791179f47487744158ecc6c4575ecc07fe0aa6
alexglazyrin <alex_glazyrin@yahoo.com>
2024-08-07 15:38:26 +0300

    KS-1-3

---
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson1
+
+fun main() {
+
+    val year = 1961
+    var hour: String = "09"
+    var minute: String = "07"
+
+    println("Время взлета:")
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    println("Время посадки: $hour:$minute")
+}
