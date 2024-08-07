267bce44c3db6f58e8cab3f377efb915c6d54848
alexglazyrin <alex_glazyrin@yahoo.com>
2024-08-07 15:42:58 +0300

    KS-1-5

---
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -0,0 +1,12 @@
+package org.example.lesson1
+
+fun main() {
+
+    val flyingGagarinInSeconds = 6480 // Нашел только про 108 минут информацию.
+    val hour = flyingGagarinInSeconds / 3600
+    val minutes = flyingGagarinInSeconds % 3600 / 60
+    val seconds = flyingGagarinInSeconds % 3600 % 60
+
+    println("Полет Ю. Гагарина длился: $hour:$minutes:$seconds")
+
+}
