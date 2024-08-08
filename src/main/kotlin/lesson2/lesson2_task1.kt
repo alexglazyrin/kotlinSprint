7d0024dc65b3d9e0bf2c4f5c7a99b2a33ff0a057
Glazyrin Alex <alex_glazyrin@yahoo.com>
2024-08-08 16:48:10 +0300

    KS-2-1 Решение задачи 1 к уроку 2

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson2
+
+fun main(){
+
+    val studentPoint1: Int = 3
+    val studentPoint2: Int = 4
+    val studentPoint3: Int = 3
+    val studentPoint4: Int = 5
+
+    val overagePoint: Double = (studentPoint1 + studentPoint2 + studentPoint3 + studentPoint4)/4.0
+    println("Overage point: $overagePoint")
+}
