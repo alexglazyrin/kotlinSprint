3992dd4a1143ccf969f7a64aa9a180481100e38b
Glazyrin Alex <alex_glazyrin@yahoo.com>
2024-08-09 13:58:08 +0300

    Поправил по замечаниям

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -1,13 +1,15 @@
 package org.example.lesson2
 
-fun main(){
+import java.lang.Math.pow
+
+fun main() {
 
     val startBalance = 70_000
     val depositPercent = 16.7
     var finalDeposit: Double = startBalance.toDouble()
+    val accrualPeriods = 1
+    val depositPeriod: Double = 20.0
 
-    for (year in 1..20){
-        finalDeposit += finalDeposit * depositPercent / 100
-        println("Сумма на вкладе после $year года: ${"%.3f".format(finalDeposit)}")
-    }
+    finalDeposit = startBalance * pow((1 + (depositPercent / 100) * accrualPeriods), depositPeriod)
+    println("Сумма на вкладе за $depositPeriod лет: ${"%.3f".format(finalDeposit)}")
 }
