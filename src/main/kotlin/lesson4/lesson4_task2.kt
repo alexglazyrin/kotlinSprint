bd5f2ca71705afb8c638ac4e29a602af43711866
Glazyrin Alex <alex_glazyrin@yahoo.com>
2024-08-13 08:24:58 +0300

    KS-4-2 Поправил по замечаниям

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -5,15 +5,13 @@ fun main() {
     val cargoWeight = 20
     val cargoVolume = 80
 
-    println(
-        "Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': " +
+    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': " +
                 "${
-                    (AVERAGE_MIN_WEIGHT < cargoWeight
-                            && cargoWeight <= AVERAGE_MAX_WEIGHT)
-                            && cargoVolume < AVERAGE_MAX_VOLUME
+                    (AVERAGE_MIN_WEIGHT < cargoWeight && 
+                            cargoWeight <= AVERAGE_MAX_WEIGHT) && 
+                            cargoVolume < AVERAGE_MAX_VOLUME
                 }"
     )
-
 }
 
 const val AVERAGE_MIN_WEIGHT = 35
