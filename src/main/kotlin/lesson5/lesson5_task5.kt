e008b68818f1360b58828507a7c999083119eeee
Glazyrin Alex <alex_glazyrin@yahoo.com>
2024-08-16 13:59:35 +0300

    KS-5-5 Еще чуть-чуть поправил по замечаниям задание 5 урок 5

--- src/main/kotlin/lesson5/lesson5_task5.kt
+++ src/main/kotlin/lesson5/lesson5_task5.kt
@@ -2,11 +2,14 @@ package org.example.lesson5
 
 fun main() {
 
+    val randomNumber = { (1..42).random().toInt() }
+
     val numbers = listOf(
-        getRandomNumber(),
-        getRandomNumber(),
-        getRandomNumber()
+        randomNumber(),
+        randomNumber(),
+        randomNumber()
     )
+    println(numbers)
 
     println("Введите три числа (разделяя пробелом): ")
 
@@ -14,18 +17,11 @@ fun main() {
     val winNumberCount = numbers.intersect(userNumbers).size
 
     when (winNumberCount) {
-        3 -> println("Вы угадали все 3 числа и выиграли джекпот!")
-        2 -> println("Вы угадали 2 числа из 3 и получаете крупный приз!")
-        1 -> println("Вы угадали 1 число из 3 и получаете утешительный приз!")
+        3 -> println("Вы угадали $winNumberCount числа и выиграли джекпот!")
+        2 -> println("Вы угадали $winNumberCount числа из 3 и получаете крупный приз!")
+        1 -> println("Вы угадали $winNumberCount число из 3 и получаете утешительный приз!")
         else -> println("Вы не угадали ни одного числа. Попробуйте еще раз!")
     }
 
     println(numbers)
 }
-
-fun getRandomNumber(): Int {
-    return (1..42).random()
-}
-
-const val MIN_NUMBER = 1
-const val MAX_NUMBER = 42
